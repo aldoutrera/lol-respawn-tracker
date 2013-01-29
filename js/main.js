409e75f30a89c3290651c6596bebf71220163f34
Aldo Utrera <Kugel85@gmail.com>
2013-01-29 12:22:22 -0600

    Testing the countdown functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+(function(){
+  var campBuff = Date.now() + 300000;
 
+  var timerId =
+    countdown(
+      campBuff,
+      function(ts) {
+        $('.myBuff .timer').html(ts.toString());
+      },
+      countdown.MINUTES|countdown.SECONDS);
+
+})(window.jQuery);
